114134c1a34296972bda45016a5e4e1155c0834d
Leo <leo.kulinski@gmail.com>
2021-10-31 05:32:35 +0100

    Detection intersection rayon / bounding box
    Ajout algorithme de projection de points donné sur l'écran, de calcul de leur enveloppe convexe, pour déterminer quels points de l'écran sont dans celle ci ou non.

--- camera.py
+++ camera.py
@@ -12,6 +12,33 @@ def normalize(vector):
         raise NullVector
     return vector / norm
 
+def find_convex_hull(points):
+    """Retourne l'enveloppe convexe 2D d'une ensemble de points sous la forme d'un np array.
+    Ces points sont listés dans un ordre trigonométrique.
+    Implémente l'algorithme naif de papier cadeau."""
+    #De toute façon, on a toujours 8 points donc assez rapide.
+    #Réécrire sans numpy ?
+    
+    if len(points) <= 1:
+        return points
+    
+    #1. Trouver le point le plus à gauche.
+    points_dict = {idx : point for idx, point in enumerate(points)}
+    first_idx, first_hull_point = min(points_dict.items(), key = lambda x : x[1][0])
+    #2 Emballer le nuage de points
+    del points_dict[first_idx]
+    idx, hull_point = min(points_dict.items(), key = lambda x : np.array([0, 1]) @ (x[1] - first_hull_point))
+    points_dict[first_idx] = first_hull_point
+    convex_hull = [first_hull_point]
+    while idx != first_idx:
+        convex_hull.append(hull_point)
+        del points_dict[idx]
+        idx, hull_point= min(points_dict.items(), key = lambda x :  (convex_hull[-2] - convex_hull[-1]) @ (x[1] - hull_point))
+        points_dict[idx] = hull_point
+
+    return np.array(convex_hull)
+    
+
 class Camera():
 
     def __init__(self):
@@ -71,6 +98,38 @@ class Camera():
         for i in range(0, self.resolution[0]):
             for j in range(0, self.resolution[1]):
                 yield (i, j)
+
+
+    def noname(self, bounding_box, debug=False):
+        #1. Find Projection of the vertices on the screen
+        const = normalize(self.screen_center - self.eye) @ self.normal
+        vertices = np.array(bounding_box.vertices)
+        direction = np.array([normalize(v - self.eye) for v in vertices])
+        projection = self.eye + (const / (direction @ self.normal) * direction.T).T
+        #We convert those global positions to local coordinates on the screen.
+        #For that, we take the pixel sized vectors of the basis of the plane
+        screen_basex = self.tangentx * self.pixel_size[0]
+        screen_basey = self.tangenty * self.pixel_size[1]
+        on_screen_projection = np.array([projection @ screen_basex, projection @ screen_basey]).T
+        #2 Find convex hull of those points in trigonometric order
+        convex_hull = find_convex_hull(on_screen_projection)
+        #3 Find all pixels left of all edges of the convex hull.
+        #Those are exactly the points inside the hull.
+        #hull_edges[i] contains the edge from the hull starting at convex_hull[i]
+        hull_edges = convex_hull[[(i+1) % len(convex_hull) for i in range(len(convex_hull))], :] - convex_hull
+        #The normal to an edge is 2D is given by swapping/multiplying by -1 its components like so:
+        normal_to_hull_edges = np.column_stack((-hull_edges[:, 1], hull_edges[:, 0]))
+        #Generate all pixels coordinates
+        screen_pixels = np.array([[[i, j] for j in range(self.screen_size[1])]for i in range(self.screen_size[0])])
+        #in_hull[i, j] tells whether pixel[i, j] is in the convex hull or not.
+        in_hull = np.full(screen_pixels.shape[:-1], True)
+        for i in range(len(convex_hull)):
+            test = (screen_pixels - convex_hull[i]) @ normal_to_hull_edges[i]
+            in_hull[test < 0] = False
+        if debug: #Shows pixels in hull.  
+            plt.imshow(in_hull.T, origin='lower')
+            plt.show()
+
 
     def compute_image(self):
         for pixel in self.pixels():
